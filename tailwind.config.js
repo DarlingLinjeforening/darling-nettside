59ef6a260c0c37bda49a6d98710a9324ad9455bb
Chris <ngo.chris@hotmail.com>
2023-09-07 15:48:05 +0200

    darling farger added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ export default {
     extend: {
       
     },
+    colors: {
+      'darling-purple': '#5D2684',
+      'darling-purple-light': '#D5CAEA',
+      'darling-secondary-purple': '#7931AC',
+      'darling-secondary-yellow': '#FFB43B',
+      'darling-secondary-orange': '#FF743E',
+      'darling-secondary-green': '#4D8426'
+    }
   },
   plugins: [],
 }
