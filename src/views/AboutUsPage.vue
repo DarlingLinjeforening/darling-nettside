5024be07ddff4bfe7fa9cdcb42319df6cbd2c6f6
Andreas Christiansen <andycchhrr@gmail.com>
2023-09-19 15:37:31 +0200

    wip

--- src/views/AboutUsPage.vue
+++ src/views/AboutUsPage.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-  <h1 class="text-3xl flex justify-center">About us</h1>
-  <headshot-comp />
+  <h1 class="text-3xl flex justify-center p-7 ">About us</h1>
+  <div  class="flex justify-around flex-row flex-wrap">
+  <HeadshotComp image = ../public/images/aaaa.jpg name="Andreas Christiansen" role="Leder av Web Komiteen" phone=96969696 mail="A@b.c"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+  <HeadshotComp image = ../public/images/aaaa.jpg name="Andreas Christiansen" role="Leder av Web Komiteen" phone=96969696 mail="A@b.c"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+  <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
+ 
+  </div>
+  
   <faq-comp />
   <lounge-comp />
   </div>
 </template>
 
 <script>
-import EventsComp from '../components/EventsComp.vue';
-import FaqComp from '../components/FaqComp.vue'
-import HeadshotComp from '../components/HeadshotComp.vue'
+import EventsComp from '../components/EventsComp.vue'
+import FaqComp from '../components/FaqComp.vue';
+import HeadshotComp from '../components/HeadshotComp.vue';
 import LoungeComp from '../components/LoungeComp.vue';
   export default {
   components: { HeadshotComp, FaqComp, LoungeComp, EventsComp },
