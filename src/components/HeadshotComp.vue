2a3ae00b9c2790834a6545c4da7c943a538dcba8
Andreas Christiansen <andycchhrr@gmail.com>
2023-09-20 18:34:36 +0200

    fikset headshots

--- src/components/HeadshotComp.vue
+++ src/components/HeadshotComp.vue
@@ -3,8 +3,8 @@
         <img :src="image" alt="Profile Image" class="object-cover w-52 h-52 border-2 border-darling-purple min-w-max"/>
         <p class="font-bold text-xl">{{ name }}</p>
         <p class="font-bold text-base">{{ role }}</p>
-        <p>Tel: {{ phone }}</p>
-        <p>Email: {{ mail }}</p>
+        <p v-if="phone">Tel: {{ phone }}</p>
+        <p v-if="mail">Email: {{ mail }}</p>
       </div>
   </template>
   
--- src/views/AboutUsPage.vue
+++ src/views/AboutUsPage.vue
@@ -1,36 +1,30 @@
 <template>
-  <div>
-  <h1 class="text-3xl flex justify-center p-7 ">About us</h1>
-  <div class="flex justify-center items-center">
-    <div  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
-    <!-- <div  class="flex justify-around flex-row flex-wrap"> -->
-    <HeadshotComp image = ../public/images/aaaa.jpg name="Andreas Christiansen" role="Leder av Web Komiteen" phone=96969696 mail="A@b.c"/>
-    <HeadshotComp image = ../public/images/mo.jpg name="Christopher Ngo" role="Leder av Bæsj Komiteen" phone=1234567 mail="n@g.o"/>
-    <HeadshotComp image = ../public/images/aaaa.jpg name="Andreas Christiansen" role="Leder av Web Komiteen" phone=96969696 mail="A@b.c"/>
-    </div>
-  </div>
-  </div>
-  
   <section>
-    <div class="ml-3 mt-3">
-      <HeaderComp h="1" text="About Us" width="10rem"/>
+    <div class="m-10 ml-6">
+      <HeaderComp h="1" text="About Us" width="10rem" />
     </div>
-  
-  <faq-comp />
-  <lounge-comp />
-  <headshots-comp />
+    <HeadshotsComp />
+    <faq-comp />
+    <lounge-comp />
   </section>
 </template>
 
 <script>
-import EventsComp from '../components/EventsComp.vue'
-import FaqComp from '../components/Faq/FaqComp.vue'
-import HeadshotComp from '../components/HeadshotComp.vue'
-import LoungeComp from '../components/LoungeComp.vue'
-import HeadshotsComp from '../components/HeadshotsComp.vue'
+import EventsComp from "../components/EventsComp.vue";
+import FaqComp from "../components/Faq/FaqComp.vue";
+import HeadshotComp from "../components/HeadshotComp.vue";
+import LoungeComp from "../components/LoungeComp.vue";
+import HeadshotsComp from "../components/HeadshotsComp.vue";
+import HeaderComp from "../components/HeaderComp.vue";
 
 export default {
-  components: { HeadshotComp, FaqComp, LoungeComp, EventsComp, HeadshotsComp },
-    
-}
+  components: {
+    HeadshotComp,
+    FaqComp,
+    LoungeComp,
+    EventsComp,
+    HeadshotsComp,
+    HeaderComp,
+  },
+};
 </script>
