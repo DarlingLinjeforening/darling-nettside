079ad65c118dcd6101660379e04338aa7eece415
Andreas Christiansen <andycchhrr@gmail.com>
2023-09-21 16:07:00 +0200

    added svg

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,5 +1,29 @@
 <template>
     <footer>
-        Komponent for Footer (FooterComp)
+        <img class="w-screen" src="../../public/images/footer-img-darling.svg" alt="Footer image">
+        <div class="flex">
+            <div class="footer__adresse">
+                <h3>Adresse</h3>
+                <p>Teknologiveien 22, 2815 Gjøvik, Innlandet</p>
+            </div>
+            <div>
+                <div>
+                    <a href="https://www.instagram.com/darling.linjeforening/"><img src="./assets/img/instagram-icon.svg" alt="Instagram ikon"></a>
+                    <a href="https://www.facebook.com/darling.linjeforening"><img src="./assets/img/facebook-icon.svg" alt="Facebook ikon"></a>
+                    <a href="https://www.linkedin.com/company/darling-linjeforening/"><img src="./assets/img/linkedin-icon.svg" alt="LinkedIn ikon"></a>
+                </div>
+                <p>Følg oss på Instagram, Facebook og LinkedIn!</p>
+            </div>
+            <div>
+                <h3>Kontakt</h3>
+                <p>kontakt@darlinglinjeforening.org</p>
+            </div>
+            <div>
+            <span id="footerBtn">
+                <img class="pil" src="./assets/img/chevronpil.svg" alt="arrow">
+            </span>
+            <p>© 2022 DARLING Linjeforening</p>
+        </div>
+        </div>
     </footer>
 </template>
