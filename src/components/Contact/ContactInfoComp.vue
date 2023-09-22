c89b4a9daffc6cb544ae03fedb5ef84a073e8d92
Haavaala <haavaala@stud.ntnu.no>
2023-09-22 12:13:43 +0200

    added icons

--- src/components/Contact/ContactInfoComp.vue
+++ src/components/Contact/ContactInfoComp.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="h-56 w-60 bg-darling-purple flex justify-center text-white rounded-xl md:h-96 md:w-80 lg:w-2/6 lg:h-31 lg:rounded-xl xl:mr-20">
+<div class="h-56 w-60 bg-darling-purple flex justify-center text-white rounded-xl md:h-96 md:w-80 lg:w-2/6 lg:h-31 lg:rounded-xl xl:mr-20">
     <div class="flex flex-col gap-3 justify-center md:gap-10 lg:gap-12 lg:text-lg">
-    <p>darling.linjeforening.org</p>
-    <p>+47 999 99 999</p>
-    <p>Darling Lounge, Mustad</p>
-    <p>Mon-Fri 12:00-14:00</p>
+        <p><i class="pi pi-envelope" style="margin-right: 8px;"></i>darling.linjeforening.org</p>
+        <p><i class="pi pi-phone" style="margin-right: 8px;"></i>+47 999 99 999</p>
+        <p><i class="pi pi-map-marker" style="margin-right: 8px;"></i>Darling Lounge, Mustad</p>
+        <p><i class="pi pi-clock" style="margin-right: 8px;"></i>Mon-Fri 12:00-14:00</p>
     </div>
-    </div>
+</div>
+
 </template>
 
 <script>
