fcbaa2b904b2a42b495d1fb7c573539703dea4ab
Haavaala <haavaala@stud.ntnu.no>
2023-09-26 17:59:17 +0200

    fixed width

--- src/components/Contact/ContactInfoComp.vue
+++ src/components/Contact/ContactInfoComp.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="h-56 w-60 bg-darling-purple flex justify-center text-white rounded-xl md:h-96 md:w-80 lg:w-2/6 lg:h-31 lg:rounded-xl xl:mr-20">
+<div class="h-56 w-72 bg-darling-purple flex justify-center text-white rounded-xl md:h-96 md:w-80 lg:w-2/6 lg:h-31 lg:rounded-xl xl:mr-20">
     <div class="flex flex-col gap-3 justify-center md:gap-10 lg:gap-12 lg:text-lg">
         <p><i class="pi pi-envelope" style="margin-right: 8px;"></i>{{contact.email}}</p>
         <p><i class="pi pi-phone" style="margin-right: 8px;"></i>{{contact.telephone}}</p>
