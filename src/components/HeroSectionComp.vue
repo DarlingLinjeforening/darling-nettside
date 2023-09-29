acd27db7ca90361ec2b1efcee36ff3c2a6365c0f
Haavaala <haavaala@stud.ntnu.no>
2023-09-29 10:32:50 +0200

    some adjustments

--- src/components/HeroSectionComp.vue
+++ src/components/HeroSectionComp.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="lg:flex lg:flex-row lg:justify-between lg:h-[90vh]">
-    <div class="flex justify-center flex-col lg:max-w-[40%] lg:h-[30%]">
-    <p class="pl-5 pr-5 pb-3 flex justify-center">The student organization representing the design courses at NTNU Gjøvik. Whether you're a graphic designer, interaction designer, web developer, or simply just passionate about design, Darling is your gateway to success.</p>
+    <div class="flex justify-center flex-col lg:max-w-[40%] lg:h-[20%]">
+    <p class="pl-5 pr-5 pb-3 flex justify-center xl:text-xl xl:max-w-[60ch]">The student organization representing the design courses at NTNU Gjøvik. Whether you're a graphic designer, interaction designer, web developer, or simply just passionate about design, Darling is your gateway to success.</p>
     <hero-section-button-comp-vue></hero-section-button-comp-vue>
     </div>
 <div class="w-full flex justify-center items-center flex-col gap-5 p-5 relative lg:h-[100%]">
     
-  <img class="lg:absolute lg:top-0 lg:w-80 lg:ml-10 lg:z-10 lg:mt-[150px] lg:border xl:min-w-[250px] xl:mt-[180px]" src="/images/hell.jpg" alt="hellstrøm">
-  <img class="lg:absolute lg:top-0 z-0 lg:w-80 lg:border xl:min-w-[300px]" src="/images/petter.jpg" alt="petter">
-  <img class="lg:absolute lg:top-0 lg:ml-80 lg:z-10 lg:w-72 lg:mt-32 lg:border xl:min-w-[120px] xl:ml-[450px] xl:mt-[100px]" src="/images/e40.png" alt="e40">
+  <img class="lg:absolute lg:top-0 lg:w-80 lg:ml-10 lg:z-10 lg:mt-[150px] lg:border xl:min-w-[250px] xl:mt-[180px] 2xl:mt-[280px] 2xl:min-w-[500px]" src="/images/hell.jpg" alt="hellstrøm">
+  <img class="lg:absolute lg:top-0 z-0 lg:w-80 lg:border xl:min-w-[300px] 2xl:min-w-[500px]" src="/images/petter.jpg" alt="petter">
+  <img class="lg:absolute lg:top-0 lg:ml-80 lg:z-10 lg:w-72 lg:mt-32 lg:border xl:min-w-[120px] xl:ml-[450px] xl:mt-[100px] 2xl:min-w-[400px] 2xl:ml-[500px]" src="/images/e40.png" alt="e40">
 </div>
 
 
