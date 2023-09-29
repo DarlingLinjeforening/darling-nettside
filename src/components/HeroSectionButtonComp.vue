d3f55d5829ddf39764bea8e5aaae7cea63316d50
Haavaala <haavaala@stud.ntnu.no>
2023-09-29 14:22:39 +0200

    navigating for button

--- src/components/HeroSectionButtonComp.vue
+++ src/components/HeroSectionButtonComp.vue
@@ -1,7 +1,9 @@
 <template>
+<router-link to="/events">
 <button class=" bg-darling-purple text-white hover:bg-darling-purple-light hover:text-darling-purple border-2 border-darling-purple font-bold py-2 px-4 m-5 rounded w-44">
   Explore Events <i class="pi pi-calendar"></i>
 </button>
+</router-link>
 </template>
 
 <script>
