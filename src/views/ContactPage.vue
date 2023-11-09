23589ccc6a42b45ccc9c60612ea0a50c125d2ae1
Haavaala <haavaala@stud.ntnu.no>
2023-11-09 13:59:14 +0100

    contact header

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -3,7 +3,7 @@
   <div class="w-full lg:text-xl ">
     <aside class="md:bg-darling-purple-light md:w-25 md:min-h-full md:right-0 md:absolute z-0"></aside>
     <div class="p-5">
-      <HeaderComp h="1" text="This is Darling" width="10rem" />
+      <HeaderComp h="1" text="Contact" width="10rem" />
     </div>
     <p class="flex justify-center p-5 md:justify-start md:w-1/2">
       You can contact us by visiting us, calling us, or sending an email with
