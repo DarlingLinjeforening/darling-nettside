205e6d46f79ec2417a981d44882107d4c0d9a421
ingring <ingrid.gladheim@student.uib.no>
2023-11-17 13:55:37 +0100

    studyPrograms sanity

--- src/components/StudyPrograms/StudyProgramComp.vue
+++ src/components/StudyPrograms/StudyProgramComp.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-90">
+    <div class="flex flex-col  justify-between max-w-sm p-6 bg-white  rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700">        
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900  ">
           {{ studyProgram }}
         </h5>
         <p class="text-gray-700 dark:text-gray-400 mb-5">
@@ -11,7 +11,7 @@
         </a>
       </div>
 </template>
-<script>
+<script>    
   export default {
     props: {
       studyProgram: String,
