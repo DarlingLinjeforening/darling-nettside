4ab63fe22cf2f7312e1a785910aefb56423ccae9
ingring <ingrid.gladheim@student.uib.no>
2023-11-24 13:15:19 +0100

    No icon on full time and part time

--- src/components/JobOffers/JobComp.vue
+++ src/components/JobOffers/JobComp.vue
@@ -25,19 +25,11 @@
           <span>|</span>
           <div>
             <div v-if="jobType === 'fullTime'" class="flex">
-              <div class="px-2">
-                <svg width="16" height="16" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M6.24829 2C12.8407 2 12.4012 11.3027 6.24829 11.3027" stroke="#895BB4" stroke-width="4"/>
-                <path d="M6.78101 2C0.188575 2 0.628071 11.3027 6.78101 11.3027" stroke="#895BB4" stroke-width="4"/>
-                </svg>
-              </div>
+              <!-- <div class="px-2 py-0.5"></div> -->
               <span>Full time</span>
             </div>
             <div v-else-if="jobType === 'partTime'" class="flex">
-              <svg width="16" height="16" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.24829 2C12.8407 2 12.4012 11.3027 6.24829 11.3027" stroke="#895BB4" stroke-width="4"/>
-              <path d="M6.78101 2C0.188575 2 0.628071 11.3027 6.78101 11.3027" stroke="#895BB4" stroke-width="4"/>
-              </svg>
+              <!-- <div class="px-2 py-0.5"></div> -->
               <span>Part time</span>
             </div>
           </div>
