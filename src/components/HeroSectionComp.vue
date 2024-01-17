73ad48b173f238fcd92806eac46274862a3cc84e
Haavaala <haavaala@stud.ntnu.no>
2024-01-17 13:34:23 +0100

    gjorde noe

--- src/components/HeroSectionComp.vue
+++ src/components/HeroSectionComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lg:flex lg:flex-row lg:justify-between lg:h-[90vh] ">
-    <div class="flex justify-center h-full flex-col lg:max-w-[40%] lg:h-[20%]">
+    <div class="flex justify-center h-full flex-col lg:max-w-[40%] lg:h-[40%]">
       <p class="pl-5 pr-5 pb-3 flex justify-center xl:mt-10 xl:text-xl xl:max-w-[60ch]">
         The student organization representing the design courses at NTNU Gjøvik.
         Whether you're a graphic designer, interaction designer, web developer,
@@ -13,17 +13,17 @@
       class="hidden lg:w-full lg:flex lg:justify-center lg:items-center lg:right-0 flex-col gap-5 p-5 relative lg:h-[100%]"
     >
       <img
-        class=" hidden lg:flex lg:absolute lg:top-0 lg:w-80 lg:ml-10 lg:z-10 lg:mt-[150px] lg:border-2 lg:border-darling-purple xl:min-w-[250px] xl:mt-[180px] 2xl:mt-[280px] 2xl:min-w-[500px]"
+        class=" hidden lg:flex lg:absolute lg:top-0 lg:w-80 lg:ml-10 lg:z-10 lg:mt-[150px] lg:border-2 lg:border-darling-purple xl:min-w-[120px] xl:mt-[180px] 2xl:mt-[280px] 2xl:min-w-[250px]"
          :src="urlFor(heroImages.image1)"
         alt="image1"
       />
       <img
-        class=" hidden lg:flex lg:absolute lg:top-0 z-0 lg:w-80 lg:border-2 lg:border-darling-purple xl:min-w-[300px] 2xl:min-w-[500px]"
+        class=" hidden lg:flex lg:absolute lg:top-0 z-0 lg:w-80 lg:border-2 lg:border-darling-purple xl:min-w-[120px] 2xl:min-w-[250px]"
         :src="urlFor(heroImages.image2)"
         alt="image2"
       />
       <img
-        class="hidden lg:flex lg:absolute lg:top-0 lg:ml-[300px] lg:z-10 lg:w-72 lg:mt-[100px] lg:border-2 lg:border-darling-purple xl:min-w-[120px] xl:ml-[450px] xl:mt-[100px] 2xl:min-w-[400px] 2xl:ml-[620px]"
+        class="hidden lg:flex lg:absolute lg:top-0 lg:ml-[300px] lg:z-10 lg:w-72 lg:mt-[100px] lg:border-2 lg:border-darling-purple xl:min-w-[120px] xl:ml-[450px] xl:mt-[100px] 2xl:min-w-[250px] 2xl:ml-[620px]"
         :src="urlFor(heroImages.image3)"
         alt="image3"
       />
